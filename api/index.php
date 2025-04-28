533a1c0e8c1820c876279486e348d14714c394b2
Syntheticgirl <syntheticgirls@gmail.com>
2025-04-29 04:36:37 +0900

    update

--- api/index.php
+++ api/index.php
@@ -1,4 +1,5 @@
-<?php  
-// Vercel用のエントリーポイント  
-// Petit Noteのindex.phpを読み込む  
-require __DIR__ . 'public/index.php';
+<?php
+// api/index.php  --- Serverless Function のエントリーポイント
+// Petit Note のソースは public 側に置いたままでも OK
+chdir(__DIR__ . '/../public');  // 作業ディレクトリを変更
+require 'index.php';            // Petit Note 本体を読み込む
